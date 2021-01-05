52f54b24316b889db89da532a7c453cd1fc3f684
yousefaljadi <yaljadi97@outlook.com>
2021-01-05 21:41:30 +0300

    cw7

--- question1.js
+++ question1.js
@@ -1,1 +1,9 @@
-// قم بحل السؤال الأول هنا
+let pets= [`kasko`,`chalb`,`thab`,`faar`,`zraafa`];
+console.log(pets);
+pets.pop();
+console.log(pets);
+pets.push(`jarbo3`);
+console.log(pets[4]);
+let pets1=prompt("change the name on index 2 ");
+pets.push(pets);
+console.log[pets];
